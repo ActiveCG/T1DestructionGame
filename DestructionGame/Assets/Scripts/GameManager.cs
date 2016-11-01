13ba584e337c4a94c568a4af4e8ae66b49d5d59e
ActiveCG <sivers12@student.aau.dk>
2016-11-01 15:17:03 +0100

    managers draft

---
+++ DestructionGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager {
+
+	private static GameManager _instance;
+
+	private static string GAME_SCENE = "GameScene";
+
+	private GameObject _player;
+
+	//getters:
+	public static GameManager instance{
+		get {
+			if (_instance == null)
+				_instance = new GameManager ();
+			return _instance;
+		}
+	}
+
+	public GameObject player{
+		get {
+			if (_player == null)
+				_player = GameObject.FindGameObjectWithTag("Player");
+			return _player;
+		}
+	}
+
+	//scene management
+
+	//delegates
+
+}
