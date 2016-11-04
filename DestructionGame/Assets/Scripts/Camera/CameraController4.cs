e3fc59b8c01035734f7fc23f3b6693c9907364da
Nicolaj Evers <nicolajevers@hotmail.com>
2016-11-04 15:01:06 +0100

    Camera

--- DestructionGame/Assets/Scripts/Camera/CameraController4.cs
+++ DestructionGame/Assets/Scripts/Camera/CameraController4.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController4 : MonoBehaviour {
+public class CameraController4 : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [Tooltip("The smoothness of the camera movement")]
+    [SerializeField]
+    private int smoothness = 3;
+    [Tooltip("Offset in the Y")]
+    [SerializeField]
+    private float cameraHeight = 5;
+    [Tooltip("Offset in the Z")]
+    [SerializeField]
+    private float cameraLength = 8;
+
+    private float offsetZ;
+    private float offsetY;
+
+    private GameObject player;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void LateUpdate()
+    {
+        Vector3 targetCamPos = player.transform.position + new Vector3(0, cameraHeight, -cameraLength);
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime * smoothness);
+    }
+    void FixedUpdate()
+    {
+        if (Physics.Linecast(player.transform.position, transform.position, out hit))
+        {
+            offsetZ = hit.distance;
+            offsetY = player.transform.position.y;
+        }
+    }
 }
